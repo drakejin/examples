1b123a4e00d366157831a316a1edaa19c872c420
DrakeJin <dydwls121200@gmail.com>
2020-10-06 23:26:36 +0900

    feat(kafka): add example kafka playground examples

--- kafka-playground/README.md
+++ kafka-playground/README.md
@@ -8,3 +8,8 @@ https://docs.confluent.io/current/connect/references/restapi.html
 #### kafka datagen
 
 https://www.confluent.io/blog/easy-ways-generate-test-data-kafka/
+
+#### kafka consuming
+
+https://team-platform.tistory.com/37
+
--- kafka-playground/scripts/create.produce-custom.articles.sh
+++ kafka-playground/scripts/create.produce-custom.articles.sh
@@ -1,9 +1,9 @@
 docker-compose exec kafka-connect-datagen \
   curl -X POST -H "Content-Type: application/json" \
-  --data @/usr/share/confluent-hub-components/confluentinc-kafka-connect-datagen/etc/custom/articles.config \
+  --data @/usr/share/confluent-hub-components/confluentinc-kafka-connect-datagen/etc/custom/articles.config.json \
   http://localhost:8083/connectors
 
-cat /usr/share/confluent-hub-components/confluentinc-kafka-connect-datagen/etc/custom/articles.config
+cat /usr/share/confluent-hub-components/confluentinc-kafka-connect-datagen/etc/custom/articles.config.json
 
 
 docker-compose exec kafka-connect-datagen \
